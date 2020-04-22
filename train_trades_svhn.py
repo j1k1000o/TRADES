6144c1909a3785a2fb86e3dc6399eafa89bc13cc
juancprzs <jc.perez13@uniandes.edu.co>
2020-04-22 07:30:29 -0500

    fix SVHN instantiation

--- train_trades_svhn.py
+++ train_trades_svhn.py
@@ -64,9 +64,9 @@ transform_train = transforms.Compose([
 transform_test = transforms.Compose([
     transforms.ToTensor(),
 ])
-trainset = torchvision.datasets.SVHN(root='../data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.SVHN(root='../data', split='train', download=True, transform=transform_train)
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, **kwargs)
-testset = torchvision.datasets.SVHN(root='../data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.SVHN(root='../data', split='test', download=True, transform=transform_test)
 test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
 
