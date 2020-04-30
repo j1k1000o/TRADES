9526d1ac3c4af6378039f9e6c54a492cc7759bb5
juancprzs <jc.perez13@uniandes.edu.co>
2020-04-30 18:54:53 -0500

    remove unuused funs

--- pgd_attack_generic.py
+++ pgd_attack_generic.py
@@ -36,37 +36,6 @@ def _pgd_whitebox(model, X, y, epsilon, num_steps, step_size, device):
     return err, err_pgd
 
 
-def _pgd_blackbox(model_target,
-                  model_source,
-                  X,
-                  y,
-                  epsilon=args.epsilon,
-                  num_steps=args.num_steps,
-                  step_size=args.step_size):
-    out = model_target(X)
-    err = (out.data.max(1)[1] != y.data).float().sum()
-    X_pgd = Variable(X.data, requires_grad=True)
-    if args.random:
-        random_noise = torch.FloatTensor(*X_pgd.shape).uniform_(-epsilon, epsilon).to(device)
-        X_pgd = Variable(X_pgd.data + random_noise, requires_grad=True)
-
-    for _ in range(num_steps):
-        opt = optim.SGD([X_pgd], lr=1e-3)
-        opt.zero_grad()
-        with torch.enable_grad():
-            loss = nn.CrossEntropyLoss()(model_source(X_pgd), y)
-        loss.backward()
-        eta = step_size * X_pgd.grad.data.sign()
-        X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
-        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
-        X_pgd = Variable(X.data + eta, requires_grad=True)
-        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
-
-    err_pgd = (model_target(X_pgd).data.max(1)[1] != y.data).float().sum()
-    print('err pgd black-box: ', err_pgd)
-    return err, err_pgd
-
-
 def eval_adv_test_whitebox(model, device, test_loader, epsilon, num_steps, step_size):
     """
     evaluate model by white-box attack
@@ -88,27 +57,6 @@ def eval_adv_test_whitebox(model, device, test_loader, epsilon, num_steps, step_
     clean_acc = 1.0 - natural_err_total/n_total
     pgd_acc = 1.0 - robust_err_total/n_total
     return clean_acc, pgd_acc
-
-
-def eval_adv_test_blackbox(model_target, model_source, device, test_loader):
-    """
-    evaluate model by black-box attack
-    """
-    model_target.eval()
-    model_source.eval()
-    robust_err_total = 0
-    natural_err_total = 0
-
-    for data, target in test_loader:
-        data, target = data.to(device), target.to(device)
-        # pgd attack
-        X, y = Variable(data, requires_grad=True), Variable(target)
-        err_natural, err_robust = _pgd_blackbox(model_target, model_source, X, y)
-        robust_err_total += err_robust
-        natural_err_total += err_natural
-    print('natural_err_total: ', natural_err_total)
-    print('robust_err_total: ', robust_err_total)
-
 
 def main():
     parser = argparse.ArgumentParser(description='PyTorch CIFAR PGD Attack Evaluation')
